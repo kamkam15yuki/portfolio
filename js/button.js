43c67e85bbc4c7df2cf59df16ecab98f4ebd38fb
kamkam15yuki <131838408+kamkam15yuki@users.noreply.github.com>
2024-02-25 03:14:20 +0900

    ヘッダー変更、ページの追加

--- js/button.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("sab01Button").addEventListener("click", function() {
-    this.style.backgroundColor = "#f6f6f6"; /* ボタンをクリックしたときの背景色 */
-    this.style.color = "#5c5c5c"; /* ボタンをクリックしたときのテキスト色 */
-  });
